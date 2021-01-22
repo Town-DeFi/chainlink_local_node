30799030d3fe1834a5c631e476ea254d20b59ded
szichedelic <jaredszi@gmail.com>
2021-01-22 14:23:08 -0800

    Update chown statement in SSL gen script

--- ca/genSSL.js
+++ ca/genSSL.js
@@ -4,7 +4,7 @@ exec(`
 		openssl req -new -text -passout pass:abcd -subj /CN=localhost -out ./server.req -keyout privkey.pem &&
 		openssl rsa -in privkey.pem -passin pass:abcd -out server.key &&
 		openssl req -x509 -in server.req -text -key server.key -out server.crt &&
-		chmod 600 server.key && sudo chown 70 server.key; rm privkey.pem server.req`
+		chmod 600 server.key && sudo chown $USER server.key; rm privkey.pem server.req`
 		, (error, _, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
